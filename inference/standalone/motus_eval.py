96c977ab9b754a5d0bc6d60a13dca99685137c60
QuanyiLi <quanyili0057@gmail.com>
2026-02-22 09:09:39 +0000

    devi

--- inference/standalone/motus_eval.py
+++ inference/standalone/motus_eval.py
@@ -230,7 +230,8 @@ def main():
                 state = obs[obs_state_key].cpu().numpy() if torch.is_tensor(obs[obs_state_key]) else obs[obs_state_key]
 
                 # Run inference
-                action_to_take = policy.act(batched_images, state, obs["task"][:1].tolist() * 12)
+                t = obs["task"][:1].tolist() * 12
+                action_to_take = policy.act(batched_images, state, torch.tensor(t, device=state.device))
                 # Ensure action matches torch tensor expected by the environment
                 action_to_take = torch.from_numpy(action_to_take).float().to(
                     "cuda" if torch.cuda.is_available() else "cpu")
